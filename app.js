6052a3d551d0f7dcc393ae05af30ac12a405f2ba
jvazquez <jvazquez@ucm.es>
2026-03-17 19:23:33 +0100

    Primer commit del proyecto SimpleMvcApp

---
+++ app.js
@@ -0,0 +1,11 @@
+﻿const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("Aplicacion de ejemplo para CI con Docker en Azure");
+    res.end();
+});
+
+server.listen(3001);
+
+console.log("Servidor ejecutandose en puerto 3001");
